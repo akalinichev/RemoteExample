b93958c8484a2c058a76b23c88d21dc41e79171a
Alex <a_kalinichev@hotmail.com>
2015-08-28 20:08:54 -0700

    Adding App class 2
    Signed-off-by: Alex <a_kalinichev@hotmail.com>

---
+++ RemoteExample/src/App.java
@@ -0,0 +1,17 @@
+
+public class App
+{
+
+	public App()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("omg!");
+		
+
+	}
+
+}
